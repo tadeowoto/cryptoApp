ffb7b30240dd46ce09ce4f9cb55bc01002a594c5
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-02-26 10:44:01 -0300

    tailwind colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <article>
+      <h1 className="text-3xl font-bold underline">Hello</h1>
+    </article>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "dark-bg": "#192331", // Fondo principal
+        "grid-bg": "#252F3E", // Fondo para bentogrids
+        highlight: "#3FDBAF", // Verde agua para resaltar
+        "text-white": "#FFFFFF", // Letras blancas
+        success: "#00E6A8", // Verde para subidas
+        failure: "#FF5757",
       },
     },
   },
